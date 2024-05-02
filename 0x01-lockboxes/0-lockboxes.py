49609b0caf0edae60f5739a2455d26de4cc371ff
seifbenothman <seifbenothman95@gmail.com>
2024-05-02 10:24:24 +0000

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Lockbox"""
+
+
+def canUnlockAll(boxes):
+    """Attempting to Unlock"""
+    if not boxes:
+        return False
+
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    queue = [0]
+
+    while queue:
+        current_box = queue.pop(0)
+        for key in boxes[current_box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    return all(visited)
