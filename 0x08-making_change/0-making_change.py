e9438316c3376df27b315f145571d7cd50af0dd6
seifbenothman <seifbenothman95@gmail.com>
2024-06-20 18:38:18 +0000

    0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module for making change
+"""
+
+
+def makeChange(coins, total):
+    """
+    Determine the fewest number of coins needed to meet a given amount total.
+    """
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            if dp[amount - coin] != float('inf'):
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
