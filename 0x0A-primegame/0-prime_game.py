43aed12479bfd83cc17aca654d96df15e34746f5
seifbenothman <seifbenothman95@gmail.com>
2024-07-02 23:24:05 +0000

    0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def sieve_of_eratosthenes(n):
-    """Return a list of primes up to n (inclusive) using the Sieve."""
+    """Return a list of primes up to n (inclusive) using the Sieve of Eratosthenes."""
     is_prime = [True] * (n + 1)
     p = 2
     while p * p <= n:
@@ -11,6 +11,15 @@ def sieve_of_eratosthenes(n):
         p += 1
     return [p for p in range(2, n + 1) if is_prime[p]]
 
+def count_prime_moves(n, primes):
+    """Return the number of moves that can be made with the primes up to n."""
+    count = 0
+    for prime in primes:
+        if prime <= n:
+            count += 1
+        else:
+            break
+    return count
 
 def isWinner(x, nums):
     if x < 1 or not nums:
@@ -18,22 +27,17 @@ def isWinner(x, nums):
 
     max_num = max(nums)
     primes = sieve_of_eratosthenes(max_num)
-    prime_count = [0] * (max_num + 1)
-
-    for i in range(1, max_num + 1):
-        prime_count[i] = prime_count[i - 1]
-        if i in primes:
-            prime_count[i] += 1
-
+    
     maria_wins = 0
     ben_wins = 0
-
+    
     for n in nums:
-        if prime_count[n] % 2 == 0:
+        prime_moves = count_prime_moves(n, primes)
+        if prime_moves % 2 == 0:
             ben_wins += 1
         else:
             maria_wins += 1
-
+    
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
@@ -41,6 +45,6 @@ def isWinner(x, nums):
     else:
         return None
 
-
+# Example usage
 if __name__ == "__main__":
     print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
