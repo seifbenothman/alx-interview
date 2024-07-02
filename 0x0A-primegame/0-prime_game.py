830522f3df1d68daa8b9cbf2352366d831b264a8
seifbenothman <seifbenothman95@gmail.com>
2024-07-02 23:12:17 +0000

    0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,58 +1,45 @@
 #!/usr/bin/python3
-def sieve_of_eratosthenes(max_num):
-    """ Generate a list of primes up to max_num using the Sieve. """
-    is_prime = [True] * (max_num + 1)
-    is_prime[0], is_prime[1] = False, False
+
+def sieve_of_eratosthenes(n):
+    """Return a list of primes up to n (inclusive) using the Sieve of Eratosthenes."""
+    is_prime = [True] * (n + 1)
     p = 2
-    while (p * p <= max_num):
+    while p * p <= n:
         if is_prime[p]:
-            for i in range(p * p, max_num + 1, p):
+            for i in range(p * p, n + 1, p):
                 is_prime[i] = False
         p += 1
-    return [p for p in range(max_num + 1) if is_prime[p]]
-
-
-def optimal_play(primes, n):
-    """ Determine if the player can make a move given the current state. """
-
-    return any(prime <= n for prime in primes)
-
+    return [p for p in range(2, n + 1) if is_prime[p]]
 
 def isWinner(x, nums):
-    """ Determine the winner of the prime game. """
     if x < 1 or not nums:
         return None
-
+    
     max_num = max(nums)
     primes = sieve_of_eratosthenes(max_num)
+    prime_count = [0] * (max_num + 1)
+
+    for i in range(1, max_num + 1):
+        prime_count[i] = prime_count[i - 1]
+        if i in primes:
+            prime_count[i] += 1
+    
     maria_wins = 0
     ben_wins = 0
-
+    
     for n in nums:
-        current_player = 'Maria'
-        while True:
-            if not optimal_play(primes, n):
-                if current_player == 'Maria':
-                    ben_wins += 1
-                else:
-                    maria_wins += 1
-                break
-
-            for prime in primes:
-                if prime <= n:
-                    n -= prime
-                    n //= prime
-                    break
-
-            current_player = 'Ben' if current_player == 'Maria' else 'Maria'
-
+        if prime_count[n] % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+    
     if maria_wins > ben_wins:
-        return 'Maria'
+        return "Maria"
     elif ben_wins > maria_wins:
-        return 'Ben'
+        return "Ben"
     else:
         return None
 
-
+# Example usage
 if __name__ == "__main__":
     print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
